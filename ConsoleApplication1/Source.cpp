566949d52bfee72f77738d2f051506eb00b3bb8d
PedroFaria <pedrommlcfaria@gmail.com>
2016-11-04 17:51:54 +0000

    completly different approach

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -8,6 +8,10 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/video/background_segm.hpp>
 
+#include <stdio.h>
+#include <tchar.h>
+
+
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/features2d/features2d.hpp"
@@ -22,6 +26,8 @@
 
 using namespace std;
 using namespace cv;
+
+Mat src, src_gray, bckg, bckg_gray, dest;
 
 /// Global variables
 vector<Image> images= vector<Image>();
@@ -75,24 +81,65 @@ void updateBlobParams(int, void*) {
 	imshow("keypoints", im_with_keypoints);
 }
 
-Mat src, src_gray, bckg, bckg_gray, dest;
 
 int main(int, char** argv){
 
 	// Read image
-	src = imread("../data/PKlot_FPR05_subset/2013-03-05_08_20_02.jpg");
+	src = imread("../data/FEUP/teste.jpg");
+	//src = imread("../data/PKlot_FPR05_subset/2013-03-05_08_20_02.jpg");
 	cvtColor(src, src_gray, CV_BGR2GRAY);
+	//imshow("origin", src);
+	/*
+	//background subtration
+	Mat srcb = imread("../data/PKlot_FPR05_subset/2013-03-09_08_50_03.jpg");
+	bckg = imread("../data/PKlot_FPR05_subset/2013-03-09_07_15_01.jpg");
+	cvtColor(bckg, bckg_gray, CV_BGR2GRAY);
+	//BckgSubt sub = BckgSubt(src_gray, bckg_gray);
+	//sub.apply();
+	//dest = sub.getrmbckg();
+	//imshow("sub", dest);
+	*/
 
 
 	Morf_operations mf;
 
 	//opening
-	Mat g_eroded = mf.Erosion(src_gray,1,4);
-	Mat g_dilated = mf.Dilation(g_eroded,1,4);
+	Mat g_eroded = mf.Erosion(src_gray,1,2);
+	Mat g_dilated = mf.Dilation(g_eroded,1,2);
 	imshow("opening gray", g_dilated);
+	//para os carros pretos
+	Mat EH;
+	equalizeHist(g_dilated, EH);
+	imshow("equa hist", EH);
 
 	Mat gbh = mf.BlackHat(src_gray);
 
+	/*
+	BckgSubt sub2 = BckgSubt(g_dilated, bckg_gray);
+	sub2.apply();
+	Mat dest2 = sub.getrmbckg();
+	imshow("sub2", dest2);
+	*/
+
+	Canny(g_dilated, g_dilated, 100, 200, 3);
+	/// Find contours   
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	RNG rng(12345);
+	findContours(g_dilated, contours, hierarchy, CV_RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+	/// Draw contours
+	Mat drawing = Mat::zeros(g_dilated.size(), CV_8UC3);
+	for (int i = 0; i< contours.size(); i++)
+	{
+		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+	}
+
+	imshow("Result window", drawing);
+
+
+
+	/*
 	SurfFeatureDetector detectorS(800);
 
 	vector<KeyPoint> keypoints_1, keypoints_2, keypoints_3;
@@ -122,8 +169,14 @@ int main(int, char** argv){
 	extractor.compute(gbh, keypoints_3, descriptors_3);
 
 	
+	*/
 
+	/*
+	Mat gbh = mf.BlackHat(src_gray);
+	imshow("gray blackhat", gbh);
+	*/
 
+	/*
 	// Set up the detector with default parameters.
 	SimpleBlobDetector detector;
 
@@ -138,7 +191,7 @@ int main(int, char** argv){
 
 	// Show blobs
 	//imshow("keypoints", im_with_keypoints);
-	
+	*/
 
 	/* BLOB  */
 	/*
